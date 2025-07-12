43ecdf5f9520b3b966ea22051c9a5553b294977d
Upendra Singh Tomar <tomer.upendra01@gmail.com>
2025-07-12 16:36:09 +0530

    feat: add MUI and Emotion for styling
    - Added @emotion/react and @emotion/styled dependencies for CSS-in-JS styling.
    - Integrated MUI (Material-UI) with a default theme in the main application file.
    - Wrapped the App component with ThemeProvider and included CssBaseline for consistent styling.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store.ts";
 import App from "./App.tsx";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const theme = createTheme(); // Default MUI theme
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>
 );
